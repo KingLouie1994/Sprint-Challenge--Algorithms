9a5db4b90ec7b3cb3f4ebb6d335a53eb089b0145
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-04-17 09:54:51 +0200

    Recursive Count: Exercise done and all tests pass

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,17 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
+    total = 0
+    if "th" not in word:
+        return total
+    else:
+        total += 1 + count_th(word.replace("th", "lol", 1))
+    return total
     
-    # TBC
-    
-    pass
+print(count_th(""))        
+print(count_th("abcthxyz"))
+print(count_th("abcthefthghith"))
+print(count_th("thhtthht"))
+print(count_th("THtHThth"))
+
+# Source: https://www.tutorialspoint.com/python/string_replace.htm
